5f51e261f53714703a6600129a8c6cc77cc3d16f
Arjun <rao.arjun87@gmail.com>
2018-11-25 21:25:45 -0500

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  grid-column: 1 / -1;
+  margin-left: -1rem;
+  margin-right: -1rem;
+  padding: 2rem 2rem 5rem 2rem;
+  box-shadow: inset 0px -10px 30px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const Content = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+
+  a {
+    color: white;
+    &:hover {
+      opacity: 0.85;
+      color: white;
+    }
+  }
+`;
+
+const Header = ({ children }) => (
+  <Wrapper>
+    <Content>{children}</Content>
+  </Wrapper>
+);
+
+export default Header;
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.node]).isRequired,
+};
